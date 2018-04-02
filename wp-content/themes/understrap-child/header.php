327a6786992eaab3a6b17a0955064474840b1ee1
alfredop14 <alfredop>
2018-04-02 11:53:58 -0400

    Analytics code added and style fixed for beat the clock

--- wp-content/themes/understrap-child/header.php
+++ wp-content/themes/understrap-child/header.php
@@ -21,6 +21,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-103418401-4"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-103418401-4');
+	</script>
+	<!-- End - Google Analytics -->
 	<?php wp_head(); ?>
 </head>
 
