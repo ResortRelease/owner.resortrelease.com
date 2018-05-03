72e66ac54118b224d46e43dbd9a38ab85f073af6
alfredop14 <alfredop>
2018-05-03 11:18:38 -0400

    Removed <p> from mortgage 4

--- wp-content/themes/understrap-child/mortgage-4.php
+++ wp-content/themes/understrap-child/mortgage-4.php
@@ -5,9 +5,6 @@
     <hr>
     <div class="row">
       <div class="col-md-8 offset-md-2 text-justify margin-top-20">
-        <p><b>RCI is a division of the firm Wyndham Worldwide. This is currently 
-          the largest Timeshare Exchange Company in the world.</b> The Companies featured 
-          in example <b class="p-number" style="font-size:inherit;">2A & 2B</b>  are operated by the RCI</p>
       </div>
     </div>
   <div class="box-image row">
