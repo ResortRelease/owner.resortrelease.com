22e8e35950056c0053c3ae3cf4ac0c5b037210cf
alfredop14 <alfredop>
2018-07-16 14:48:19 -0400

    Edit status change from GET to POST

--- wp-content/themes/understrap-child/api/edit_user_stage.php
+++ wp-content/themes/understrap-child/api/edit_user_stage.php
@@ -5,44 +5,71 @@
     
   // }
   require 'mt.php';
-  $userEmail = $_GET['email'];
-  $crmStage = $_GET['stage'];
+  if(isset($_POST)){
+    $userEmail = $_POST['email'];
+    $crmStage = $_POST['stage'];
+  }else{
+    $userEmail = $_GET['email'];
+    $crmStage = $_GET['stage'];
+  }
   $searchUser = $contactApi->getList($userEmail);
   foreach($searchUser['contacts'] as $user){
     $userID = $user['id'];
   };
   switch($crmStage){
     case "Welcome":
+    case: 1
+    case: "1"
       $stageId = 1;
       break;
     case "Estoppel":
+    case: 2
+    case: "2"
       $stageId = 2;
       break;
     case "Inventory":
+    case: 3
+    case: "3"
       $stageId = 3;
       break;
     case "ROFR":
+    case: 4
+    case: "4"
       $stageId = 4;
       break;
     case "Deed Prep":
+    case: 5
+    case: "5"
       $stageId = 5;
       break;
     case "Doc Prep":
+    case: 6
+    case: "6"
       $stageId = 6;
       break;
     case "Execution":
+    case: 7
+    case: "7"
       $stageId = 7;
       break;
     case "Execution Received - Deeded":
+    case: 8
+    case: "8"
       $stageId = 8;
       break;
     case "Execution Received - Non- Deeded":
+    case: 9
+    case: "9"
       $stageId = 9;
       break;
     case "Finalization":
+    case: 10
+    case: "10"
       $stageId = 10;
       break;
     case "Closed":
+    case: 11
+    case: "11"
       $stageId = 11;
       break;
   }
