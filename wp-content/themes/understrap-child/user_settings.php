cc679da3cf45531169f7b1b35d221439c9652349
alfredop14 <alfredop>
2018-07-18 12:52:39 -0400

    Change Password. Margin-bottom.

--- wp-content/themes/understrap-child/user_settings.php
+++ wp-content/themes/understrap-child/user_settings.php
@@ -29,28 +29,31 @@
     color: #8ac44a;
     font-style: italic;
   }
+  #settings{
+    margin-bottom: 400px;
+  }
 </style>
-<div class="more-info margin-top-20">
+<div class="more-info margin-top-20" id="settings">
   <i><h3 class="bold-name">Settings</h3></i>
   <ul class="margin-top-20">
     <!-- <li><i class="fa fa-map-pin"></i> Change Address</li> -->
     <a href="#changePass"><li><i class="fa fa-key"></i> Change Password</li></a>
+    <div class="collapse" id="changePass">
+      <i><h4 class="bold-name">Change Password</h4></i>
+      <div id="pw-status"></div>
+      <form id="update-password" action="" method="post">
+        <div class="form-group">
+          <input type="hidden" name="action" value="patch" />
+          <input id="changePW" type="text" class=" form-control margin-top-20" placeholder="New Password" name="newpassword">
+        </div>
+        <div class="form-group">
+          <button class="form-control button success" type="submit"  name="submit" value="Change Password">Change Password</button>
+        </div>
+      </form>
+    </div>
     <!-- <li><i class="fa fa-file-text"></i> See/Upload Pending Documents</li> -->
     <li><a href="<?php echo $signout; ?>"><i class="fa fa-sign-out"></i> Log Out</a></li>
   </ul>
-</div>
-<div class="more-info margin-top-20 collapse" id="changePass">
-  <i><h4 class="bold-name">Change Password</h4></i>
-  <div id="pw-status"></div>
-  <form id="update-password" action="" method="post">
-    <div class="form-group">
-      <input type="hidden" name="action" value="patch" />
-      <input id="changePW" type="text" class=" form-control margin-top-20" placeholder="New Password" name="newpassword">
-    </div>
-    <div class="form-group">
-      <button class="form-control button success" type="submit"  name="submit" value="Change Password">Change Password</button>
-    </div>
-  </form>
 </div>
 <script>
   jQuery('a[href="#changePass"]').click(function(){
