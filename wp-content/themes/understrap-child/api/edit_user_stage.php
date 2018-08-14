3717e44d1fdfafd39d74f13257f12f53583a7df3
alfredop14 <alfredop>
2018-08-14 10:42:07 -0400

    Updating user_status. Removing comments above, creating user updating stage, removed sql connection, Post or Get, Added new fields to create contact, updated names of stages

--- wp-content/themes/understrap-child/api/edit_user_stage.php
+++ wp-content/themes/understrap-child/api/edit_user_stage.php
@@ -1,31 +1,58 @@
 <?php 
-  // if($_SERVER['HTTP_REFERER'] !== 'gooddomain.com'){
-  //   die('Unauthorized access');
-  // }else {
-    
-  // }
-  $http_origin = $_SERVER['HTTP_ORIGIN'];
+  /* CORS */
+    $http_origin = $_SERVER['HTTP_ORIGIN'];
+    $allowed_domains = array(
+      'https://www.resortrelease.com',
+      'https://www.resortreleasecrm.com'
+    );
 
-  $allowed_domains = array(
-    'https://www.resortrelease.com',
-    'https://www.resortreleasecrm.com'
-  );
-
-  if (in_array($http_origin, $allowed_domains))
-  {  
-      header("Access-Control-Allow-Origin: $http_origin");
-  }
-  require 'mt.php';
-  $userEmail = $_GET['email'];
-  $crmStage = $_GET['stage'];
+    if (in_array($http_origin, $allowed_domains))
+    {  
+        header("Access-Control-Allow-Origin: $http_origin");
+    }
+  /* END CORS */  
+  require 'mt.php'; /* Mautic API */ 
+  
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userEmail = $_POST['email'];
     $crmStage = $_POST['stage'];
+    $type = $_POST;
+  } else{
+    $type = $_GET;
+    $userEmail = $_GET['email'];
+    $crmStage = $_GET['stage'];
   }
   $searchUser = $contactApi->getList($userEmail);
   foreach($searchUser['contacts'] as $user){
     $userID = $user['id'];
   };
+  /* If Contact Does Not Exist in Mautic */
+  if (!$userID || $userID == 0) {
+    $data = array(
+      'fullname' => $type['fullname'],
+      'email' => $userEmail,
+      'phone' => $type['phone'],
+      'resort' => $type['resort'],
+      'comments' => $type['comments'],
+      'hearduson' => $type['hearduson'],
+      'utmcampaign' => $type['utm_campaign'],
+      'utmsource' => $type['utm_source'],
+      'utmad' => $type['utm_ad'],
+      'utmadgroup' => $type['utm_adgroup'],
+      'utmmedium' => $type['utm_medium'],
+      'utmplacement' => $type['utm_placement'],
+      'utmterm' => $type['utm_term'],
+      'msclkid' => $type['msclkid'],
+      'gclid' => $type['gclid'],
+      'crm_lead_source' => $type['crmleadsource'],
+      'crmleadsubsource' => $type['crmlead-subsource'],
+      'hasapp' => $type['hasapp'],
+      'hasform' => $type['hasform']
+    );
+    $newContact = $contactApi->create($data);
+    $userID = $newContact['contact']['id'];
+    $response = $stageApi->addContact($stageId, $userID);
+  }
   switch($crmStage){
     case "Welcome":
     case 1:
@@ -47,12 +74,12 @@
     case "4":
       $stageId = 4;
       break;
-    case "Deed Prep":
+    case "DeedPrep":
     case 5:
     case "5":
       $stageId = 5;
       break;
-    case "Doc Prep":
+    case "DocPrep":
     case 6:
     case "6":
       $stageId = 6;
@@ -62,12 +89,12 @@
     case "7":
       $stageId = 7;
       break;
-    case "Execution Received - Deeded":
+    case "ExecutionReceived-Deeded":
     case 8:
     case "8":
       $stageId = 8;
       break;
-    case "Execution Received - Non- Deeded":
+    case "ExecutionReceived-Non-Deeded":
     case 9:
     case "9":
       $stageId = 9;
@@ -88,36 +115,33 @@
       $stageId = 12;
       break;
   }
-    $response = $stageApi->addContact($stageId, $userID);
-    if (!isset($response['success'])) {
-      echo "User Does Not Exist in Mautic.<br>";
-      print_r($response['errors']);
-      echo "<br>";
-    }else{
-      echo $userEmail.' -> Moved to Stage -> '.$stageId.'<br><br>';
-    }
-
-    require 'ils.php';
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }     
-    //Encode $example array into a JSON string.
-    $emailEncoded = json_encode($userEmail);
-    $stageEncoded = json_encode($stageId);
-    $userEmail = (string)$userEmail;
+  $response = $stageApi->addContact($stageId, $userID);
+  if (!isset($response['success'])) {
+    echo 'User Does Not Exist in Mautic.<br>';
+  }else{
+    echo $userEmail.' -> Moved to Stage -> '.$stageId.'<br>';
+  }
+  /* Create connection
+     require 'ils.php';
+     $conn = new mysqli($servername, $username, $password, $dbname);
+      Check connection
+     if ($conn->connect_error) {
+       die("Connection failed: " . $conn->connect_error);
+     }     
+     Encode $example array into a JSON string.
+     $emailEncoded = json_encode($userEmail);
+     $stageEncoded = json_encode($stageId);
+     $userEmail = (string)$userEmail;
     
-    // Create or update client stage
-    $sql = "INSERT INTO $table (id, email, stage) VALUES ('$userID', '$userEmail', '$stageId')
-            ON DUPLICATE KEY UPDATE email = '$userEmail', stage= '$stageId'";
+      Create or update client stage
+     $sql = "INSERT INTO $table (id, email, stage) VALUES ('$userID', '$userEmail', '$stageId')
+             ON DUPLICATE KEY UPDATE email = '$userEmail', stage= '$stageId'";
 
-    if ($conn->query($sql) === TRUE) {
-      echo "Created or Updated successfully";
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+     if ($conn->query($sql) === TRUE) {
+       echo "Created or Updated successfully";
+     } else {
+       echo "Error: " . $sql . "<br>" . $conn->error;
+     }
     
-    $conn->close();
+  $conn->close(); */
 ?>
