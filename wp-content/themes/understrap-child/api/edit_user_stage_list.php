5134f635f1164fb4e32dbde464a95a06a1f2c036
alfredop14 <alfredop>
2018-09-20 10:25:31 -0400

    Removing desc order from status-list

--- wp-content/themes/understrap-child/api/edit_user_stage_list.php
+++ wp-content/themes/understrap-child/api/edit_user_stage_list.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$sql = "SELECT * FROM $table ORDER BY time DESC";
+$sql = "SELECT * FROM $table";
 $result = $conn->query($sql);
 echo "
 <style>
