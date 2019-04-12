51f105ff803d0e039aac4d4e03070825d4575170
alfredop14 <alfredop>
2019-04-12 15:20:13 -0400

    re adding vermont

--- wp-content/themes/understrap-child/transfer-7.php
+++ wp-content/themes/understrap-child/transfer-7.php
@@ -1,6 +1,6 @@
 <section>
   <div class="container text-center">
-    <h1 class="h-number">7</h1>
+    <h1 class="h-number">7A</h1>
     <h1 class="text-capitalize">new Legislation will make timeshares <span style="display: block">nearly impossible to exit</span></h1>
     <div class="row margin-top-40 scroll-div img-fluid">
       <img class="margin-auto" src="../wp-content/themes/understrap-child/assets/slides/7/legislation-important.jpg" alt="">
@@ -33,9 +33,27 @@
     </div>
   </div>
 </section>
+<section style="background: white!important;">
+  <div class="container text-center">
+    <h1 class="h-number">7B</h1>
+    <h1>Legislation Passed To Make <span style="display: inline-block">Timeshare Ownership More Difficult.</span></h1>
+    <div class="row margin-top-40">
+      <img class="margin-auto" src="../wp-content/themes/understrap-child/assets/slides/7/New-Bill-Passed.png" alt="">
+    </div>
+
+    <div class="row margin-top-40">
+      <img class="margin-auto" src="../wp-content/themes/understrap-child/assets/slides/7/Client-Mfee.png" alt="">
+    </div>
+    <h3>(Actual Client's Maintenance Fees)</h3>
+
+    <div class="row margin-top-40">
+      <img class="margin-auto" src="../wp-content/themes/understrap-child/assets/slides/7/Failed-Vermont-Bill.jpg" alt="">
+    </div>
+  </div>
+</section>
 <script>
-  jQuery(window).ready(function () {
-    changePageIndex(7, 6, 8);
-    jQuery('.red-car').css('left', '78%');
-  });
+jQuery(window).ready(function(){
+  changePageIndex(7,6,8);
+  jQuery('.red-car').css('left', '78%');        
+});
 </script>
