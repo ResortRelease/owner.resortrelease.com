14f05bcaf5ca36da7b266bf6ec864244ab85598d
alfredop14 <alfredop>
2019-04-12 15:12:00 -0400

    Upload new Transfer/#7 with new legislation.

--- wp-content/themes/understrap-child/transfer-7.php
+++ wp-content/themes/understrap-child/transfer-7.php
@@ -1,24 +1,41 @@
 <section>
   <div class="container text-center">
     <h1 class="h-number">7</h1>
-    <h1>Legislation Passed To Make <span style="display: inline-block">Timeshare Ownership More Difficult.</span></h1>
-    <div class="row margin-top-40">
-      <img class="margin-auto" src="../wp-content/themes/understrap-child/assets/slides/7/New-Bill-Passed.png" alt="">
+    <h1 class="text-capitalize">new Legislation will make timeshares <span style="display: block">nearly impossible to exit</span></h1>
+    <div class="row margin-top-40 scroll-div img-fluid">
+      <img class="margin-auto" src="../wp-content/themes/understrap-child/assets/slides/7/legislation-important.jpg" alt="">
     </div>
-
-    <div class="row margin-top-40">
-      <img class="margin-auto" src="../wp-content/themes/understrap-child/assets/slides/7/Client-Mfee.png" alt="">
+    <div class="text-left boxed margin-top-40" style="background:#6d1f1f;">
+      <h1 style="color:white;">this bill could:</h1>
+      <ul style="color:white;">
+        <li>
+          <h3 style="color:#d8d8d8;" >Guarantee <b style="color:white;">maintenance fee</b> payments <b>for life</b></h3>
+        </li>
+        <li>
+          <h3 style="color:#d8d8d8;" ><b style="color:white;">Eliminate</b> all legal exit options</h3>
+        </li>
+        <li>
+          <h3 style="color:#d8d8d8;" ><b style="color:white;">Prohibit</b> the use of consumer protection groups (exit companies)</h3>
+        </li>
+        <li>
+          <h3 style="color:#d8d8d8;" >Impose <b style="color:white;">regulations and penalties</b> toward safe timeshare exit</h3>
+        </li>
+        <li>
+          <h3 style="color:#d8d8d8;" ><b style="color:white;">Severely limit</b> owner's efforts to <b style="color:white;">sell</b></h3>
+        </li>
+        <li>
+          <h3 style="color:#d8d8d8;" >leave owner with little to <b style="color:white;">no way out</b></h3>
+        </li>
+      </ul>
     </div>
-    <h3>(Actual Client's Maintenance Fees)</h3>
-
-    <div class="row margin-top-40">
-      <img class="margin-auto" src="../wp-content/themes/understrap-child/assets/slides/7/Failed-Vermont-Bill.jpg" alt="">
+    <div class="margin-top-40">
+      <h1>You might not be able to enroll in any <br>exit services after july <span style="font-size:1.5em;">1</span>st</h1>
     </div>
   </div>
 </section>
 <script>
-jQuery(window).ready(function(){
-  changePageIndex(7,6,8);
-  jQuery('.red-car').css('left', '78%');        
-});
+  jQuery(window).ready(function () {
+    changePageIndex(7, 6, 8);
+    jQuery('.red-car').css('left', '78%');
+  });
 </script>
